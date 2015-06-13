5b0fd7b437a09970b263803849b0568abf11b4b8
Jon Eisen <jon@metricfeat.com>
2015-06-13 13:14:08 -0600

    lein new

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to pitstop
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/pitstop/core_test.clj
@@ -0,0 +1,7 @@
+(ns pitstop.core-test
+  (:require [clojure.test :refer :all]
+            [pitstop.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
