1a6c21a9daf82bf57d430600bbe9517e61a82ee7
Jon Eisen <jon@metricfeat.com>
2015-08-17 14:29:30 -0600

    Version 0.2.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.rafflecopter/pitstop "0.2.2-SNAPSHOT"
+(defproject com.rafflecopter/pitstop "0.2.2"
   :description "Meessage-deferring application"
   :url "http://github.com/Rafflecopter/pitstop"
   :license {:name "MIT"
